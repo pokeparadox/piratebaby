169c945de3465f293131981b1673a2e48835c485
pokeparadox <pokeparadox@37f7cd7a-f1f0-601d-cf3d-551a7def7f7d>
2011-05-25 04:03:57 +0000

    Added: Saving to Baby class.

--- source/Baby.cpp
+++ source/Baby.cpp
@@ -24,19 +24,83 @@
 
 #include "Baby.h"
 #include "AnimatedSprite.h"
+#include "StringUtility.h"
 using Penjin::Baby;
 using Penjin::AnimatedSprite;
 
-Baby::Baby() : sprite(NULL), intelligence(1), weight(0.2f)
+Baby::Baby() :  intelligence(1), weight(0.2f), sprite(NULL), action(ACTION_IDLE)
 {
     //ctor
     sprite = new AnimatedSprite;
+    Penjin::ERRORS e = load(DEFAULT_BABY_SAVE);
+    if(e != PENJIN_OK)
+        defaultConfig();
+    else
+    {
+        string section = "Status";
+        intelligence = StringUtility::stringToInt( getValue(section,"Intelligence", "1"));
+        weight = StringUtility::stringToFloat( getValue(section, "Weight","0.2") );
+        stringToAction(getValue(section, "Action","ACTION_IDLE"));
+    }
 }
 
 Baby::~Baby()
 {
     //dtor
     delete sprite;
+    string section = "Status";
+    setValue(section, "Intelligence", StringUtility::intToString(intelligence) );
+    setValue(section, "Weight", StringUtility::floatToString(weight) );
+    setValue(section, "Action", actionToString());
+    save(DEFAULT_BABY_SAVE);
+}
+
+void Baby::defaultConfig()
+{
+    string section = "Status";
+    setValue(section, "Intelligence", "1");
+    setValue(section, "Weight", "0.2");
+    setValue(section, "Action", "ACTION_IDLE");
+}
+
+string Baby::actionToString()
+{
+    if(action == ACTION_IDLE)
+        return "ACTION_IDLE";
+    else if(action == ACTION_DANCE)
+        return "ACTION_DANCE";
+    else if(action == ACTION_JUMP)
+        return "ACTION_JUMP";
+    else if(action == ACTION_BLINK)
+        return "ACTION_BLINK";
+    else if(action == ACTION_SLEEP)
+        return "ACTION_SLEEP";
+    else if(action == ACTION_WAKE)
+        return "ACTION_WAKE";
+    else if(action == ACTION_EVOLVE)
+        return "ACTION_EVOLVE";
+
+    return "ACTION_NONE";
+}
+
+void Baby::stringToAction(const string& s)
+{
+    if(s == "ACTION_IDLE")
+        action = ACTION_IDLE;
+    else if(s == "ACTION_DANCE")
+        action = ACTION_DANCE;
+    else if(s == "ACTION_JUMP")
+        action = ACTION_JUMP;
+    else if(s == "ACTION_BLINK")
+        action = ACTION_BLINK;
+    else if(s == "ACTION_SLEEP")
+        action = ACTION_SLEEP;
+    else if(s == "ACTION_WAKE")
+        action = ACTION_WAKE;
+    else if(s == "ACTION_EVOLVE")
+        action = ACTION_EVOLVE;
+    else
+        action = ACTION_NONE;
 }
 
 void Baby::render()
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "25";
 	static const char MONTH[] = "05";
 	static const char YEAR[] = "2011";
 	static const char UBUNTU_VERSION_STYLE[] = "11.05";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 0;
-	static const long BUILD = 362;
-	static const long REVISION = 2015;
+	static const long BUILD = 372;
+	static const long REVISION = 2068;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 514;
-	#define RC_FILEVERSION 1,0,362,2015
-	#define RC_FILEVERSION_STRING "1, 0, 362, 2015\0"
-	static const char FULLVERSION_STRING[] = "1.0.362.2015";
+	static const long BUILDS_COUNT = 523;
+	#define RC_FILEVERSION 1,0,372,2068
+	#define RC_FILEVERSION_STRING "1, 0, 372, 2068\0"
+	static const char FULLVERSION_STRING[] = "1.0.372.2068";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 362;
+	static const long BUILD_HISTORY = 372;
 	
 
 }
